7d56843d742e36727a95c2290633cc4c093521d9
Kate Donaldson <kate@katelovescode.com>
2018-05-09 13:22:49 -0500

    Tweaks the entrypoint script in case node_modules gets trashed, since it's gitignored

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 npm install -g create-react-native-app exp
-create-react-native-app myreactapp #rename your app here
-cd myreactapp/ #rename your app here
-echo "$EXPO_USER + $EXPO_PASSWORD"
+create-react-native-app myreactapp # rename your app here
+cd myreactapp/ # rename your app here
+npm install # in case you need to trash your node_module and start over
 exp login -u $EXPO_USER -p $EXPO_PASSWORD
 exp start
